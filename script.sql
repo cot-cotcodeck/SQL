8f61ec4ac7d910cc0604930feb0a2eab0db0c4d8
Audreymg <maneaudrey.a@gmail.com>
2019-12-06 13:44:47 +0100

    SQL

---
+++ script.sql
@@ -0,0 +1,11 @@
+--PARTIE 1
+--Exercice 1
+-- création d'une base de données languages
+CREATE DATABASE `languages`;
+-- création de la bd languages si elle ,n'existe pas (bonne pratique)
+CREATE DATABASE IF NOT EXISTS `languages`;
+-- Exercice 3
+CREATE DATABASE IF NOT EXISTS `frameworks` CHARACTER SET utf8
+-- Exercice 4
+DROP DATABASE `languages`
+--PARTIE 2
