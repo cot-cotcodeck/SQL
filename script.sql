b2ed7f9e925cc8c07d2d8b941d64a52f1778b972
Audreymg <maneaudrey.a@gmail.com>
2019-12-06 16:19:38 +0100

    SQL

--- script.sql
+++ script.sql
@@ -5,7 +5,14 @@ CREATE DATABASE `languages`;
 -- création de la bd languages si elle ,n'existe pas (bonne pratique)
 CREATE DATABASE IF NOT EXISTS `languages`;
 -- Exercice 3
-CREATE DATABASE IF NOT EXISTS `frameworks` CHARACTER SET utf8
+CREATE DATABASE IF NOT EXISTS `frameworks` CHARACTER SET 'utf8'
 -- Exercice 4
 DROP DATABASE `languages`
 --PARTIE 2
+-- Exercice 1
+CREATE TABLE `languages` ( `id` INT NOT NULL , `languages` VARCHAR(40) NOT NULL , PRIMARY KEY (`id`), FULLTEXT (`tool`(40))) ENGINE = MyISAM;
+-- Exercice 3
+USE `webDevelopment`;
+CREATE TABLE `tools`(id INT AUTO_INCREMENT PRIMARY KEY, tool VARCHAR(255));
+-- Exercice 4
+DROP TABLE `tools`;
